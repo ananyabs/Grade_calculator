892fa9c32afa2251acb960b3ba87170d182f983d
Ananya <bsananya22@gmail.com>
2021-05-02 12:26:03 +0530

    Grade_calculator

--- app/src/main/java/com/example/grade_calculator/MainActivity.java
+++ app/src/main/java/com/example/grade_calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Toast toast = Toast.makeText(MainActivity.this,"Progress",Toast.LENGTH_LONG);
+                toast.show();
                 markscal();
             }
+
         });
 
     }
+
 
     public void markscal()
     {
